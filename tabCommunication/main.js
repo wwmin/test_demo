ee5e496c6a8ba63041842f8c176cd7178d3e7832
wwmin <wwei.min@163.com>
2018-04-16 00:15:52 +0800

    get input value

--- tabCommunication/main.js
+++ tabCommunication/main.js
@@ -1,3 +1,17 @@
 // 使用监听 localstorage 方式通信
 window.onstorage = e => console.log(e);
 window.addEventListener("storage", e => console.log(e));
+localStorage.setItem("a", "aaaa");
+function getDom(id) {
+    return document.getElementById(id);
+}
+let checkButton = getDom("checkId");
+let setButton = getDom("setId");
+checkButton.addEventListener("click", e => {
+    let value = document.getElementById("checkInputId").value;
+    console.log(value);
+});
+setButton.addEventListener("click", e => {
+    let value = getDom("setInputId").value;
+    console.log(value);
+});
