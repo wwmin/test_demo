cb1d44049997e763f3c91e6ed448c996cb96aec3
wwmin <wwei.min@163.com>
2018-04-21 23:27:17 +0800

    install

--- service/app.js
+++ service/app.js
@@ -10,5 +10,5 @@ app.use(express.static("public"));
 var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
-    console.log("this app listening at http://%s:%s", host, port);
+    console.log("app listening at http://%s:%s", host, port);
 });
--- service/app.ts
+++ service/app.ts
@@ -1,9 +1,7 @@
 import * as express from "express";
 import "../public/libs/test.js";
 
-
 var app = express();
-
 app.get("/", function(req, res) {
   res.send("Hello world!");
 });
@@ -11,5 +9,5 @@ app.use(express.static("public"));
 var server = app.listen(3000, function() {
   var host = server.address().address;
   var port = server.address().port;
-  console.log("this app listening at http://%s:%s", host, port);
+  console.log("app listening at http://%s:%s", host, port);
 });
